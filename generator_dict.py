3699f64a169049a7387dbcdcc7651ec49eeccaca
point48 <point48@node48.feerie.lc>
2021-07-22 13:53:26 +0300

    initial commit

---
+++ generator_dict.py
@@ -0,0 +1,3 @@
+dict_obj = {'a': 'brrrrr', 'b': 2}
+print(dict_obj['a'])
+print(dict_obj.get('c', 'nema'))
---
+++ generator_list.py
@@ -0,0 +1,5 @@
+list_obj01 = [char for char in "hello"]
+list_obj01.remove("l")
+list_obj01.remove("l")
+
+print(list_obj01)
