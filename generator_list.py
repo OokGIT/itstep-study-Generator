cc6b8a26448bcb2061fc521ef96174e07656f869
point48 <point48@node48.feerie.lc>
2021-07-23 11:57:36 +0300

    Списки через 2 list объекта

--- generator_list.py
+++ generator_list.py
@@ -1,5 +1,17 @@
-list_obj01 = [char for char in "hello"]
-list_obj01.remove("l")
-list_obj01.remove("l")
+import random
+list_01 = []
+list_02 = []
+for digit in range(10):
+    digit = random.randint(1,100)
+    if digit %2 == 0:
+        list_01.append(digit)
+    else:
+        list_02.append(digit)
 
-print(list_obj01)
+print("Список с четными числами:", list_01)
+print("Список с нечетными числами:", list_02)
+print("Сумма всех четных чисел:", sum(list_01))
+print("Сумма всех нечетных чисел:", sum(list_02))
+print("Сумма чисел в обоих списках:", sum(list_01)+sum(list_02))
+
+
