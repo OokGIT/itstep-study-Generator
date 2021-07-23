9af5f8ff00a04bd3ae3993e79361b078433fdc7d
point48 <point48@node48.feerie.lc>
2021-07-23 12:06:56 +0300

    Правка синтаксиса

--- generator_dict.py
+++ generator_dict.py
@@ -1,3 +1,4 @@
-dict_obj = {'a': 'brrrrr', 'b': 2}
-print(dict_obj['a'])
-print(dict_obj.get('c', 'nema'))
+# dict_obj = {'a': 'brrrrr', 'b': 2}
+# print(dict_obj['a'])
+# print(dict_obj.get('c', 'nema'))
+
--- generator_list.py
+++ generator_list.py
@@ -1,9 +1,9 @@
 import random
 list_01 = []
 list_02 = []
-for digit in range(10):
-    digit = random.randint(1,100)
-    if digit %2 == 0:
+for x in range(10):
+    digit = random.randint(1, 100)
+    if digit % 2 == 0:
         list_01.append(digit)
     else:
         list_02.append(digit)
@@ -13,5 +13,3 @@ print("Список с нечетными числами:", list_02)
 print("Сумма всех четных чисел:", sum(list_01))
 print("Сумма всех нечетных чисел:", sum(list_02))
 print("Сумма чисел в обоих списках:", sum(list_01)+sum(list_02))
-
-
