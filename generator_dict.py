1d77e79e660c12397d00c6a7ad9915c1d6663815
point48 <point48@node48.feerie.lc>
2021-07-26 12:15:58 +0300

    Переписал задание с Dict по рекомендации преподавателя: спользовать dict[odd].append(num)

--- generator_dict.py
+++ generator_dict.py
@@ -1,17 +1,14 @@
 import random
 
-list_01 = []
-list_02 = []
+dict_even_odd = {'even': [], 'odd': [], 'sum_even': [], 'sum_odd': []}
 for x in range(10):
     digit = random.randint(1, 100)
     if digit % 2 == 0:
-        list_01.append(digit)
+        dict_even_odd['even'].append(digit)
     else:
-        list_02.append(digit)
-
-dict_even_odd = {'even': list_01, 'odd': list_02, 'sum_even': sum(list_01), 'sum_odd': sum(list_02)}
+        dict_even_odd['odd'].append(digit)
 
 print("Четные числа", dict_even_odd['even'])
 print("Нечетные числа", dict_even_odd['odd'])
-print("Сумма четных", dict_even_odd['sum_even'])
-print("Сумма нечетных", dict_even_odd['sum_odd'])
+print("Сумма четных", sum(dict_even_odd['even']))
+print("Сумма нечетных", sum(dict_even_odd['odd']))
